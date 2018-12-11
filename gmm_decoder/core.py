e1b55befb809dc64c016d694469505bd3cc7ef99
Eric Denovellis <edeno@bu.edu>
2018-12-11 09:20:57 -0500

    Add function stubs for python

---
+++ gmm_decoder/core.py
@@ -0,0 +1,54 @@
+def exact_one_step():
+    pass
+
+
+def exact_one_step_2d():
+    pass
+
+
+def gmm_approx():
+    pass
+
+
+def gmm_approx_fast():
+    pass
+
+
+def gmm_conditional():
+    pass
+
+
+def gmm_drop():
+    pass
+
+
+def gmm_drop_alpha_optimized():
+    pass
+
+
+def gmm_log_likelihood():
+    pass
+
+
+def gmm_log_likelihood_2d():
+    pass
+
+
+def gmm_mean():
+    pass
+
+
+def gmm_merge_alpha_optimized():
+    pass
+
+
+def gmm_one_step():
+    pass
+
+
+def gmm_posterior_1d():
+    pass
+
+
+def gmm_posterior_2d():
+    pass
